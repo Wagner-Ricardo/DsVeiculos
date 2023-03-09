cd13b2f44014dca930d882f9807030efec09659b
mackristofer <mackristofer@gmail.com>
2023-03-09 10:35:48 -0300

    Projeto ok

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -24,12 +24,3 @@ const Routes = () => (
   </BrowserRouter>
 );
 export default Routes;
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-ReactDOM.render(
-<React.StrictMode>
-<App />
-</React.StrictMode>,
-document.getElementById('root')
-);
